b583f4406dd50ea29717753b6fd851a8696c9b80
Aman Karmani <aman@tmm1.net>
2021-10-06 14:57:31 -0700

    package/exfatprogs: build from my fork

--- package/exfatprogs/exfatprogs.mk
+++ package/exfatprogs/exfatprogs.mk
@@ -4,11 +4,13 @@
 #
 ################################################################################
 
-EXFATPROGS_VERSION = 1.1.3
-EXFATPROGS_SOURCE = exfatprogs-$(EXFATPROGS_VERSION).tar.xz
-EXFATPROGS_SITE = https://github.com/exfatprogs/exfatprogs/releases/download/$(EXFATPROGS_VERSION)
+EXFATPROGS_VERSION = 5925eb0db78b77495af92cde1cf382c9d49cf2b8
+EXFATPROGS_SITE = $(call github,tmm1,exfatprogs,$(EXFATPROGS_VERSION))
 EXFATPROGS_LICENSE = GPL-2.0+
 EXFATPROGS_LICENSE_FILES = COPYING
+EXFATPROGS_DEPENDENCIES = host-pkgconf
+EXFATPROGS_AUTORECONF = YES
+HOST_EXFATPROGS_DEPENDENCIES = host-pkgconf
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
