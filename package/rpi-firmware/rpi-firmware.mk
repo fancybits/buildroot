8fd26c51697dc8eecd3db585dde0b084ef602d46
Aman Karmani <aman@tmm1.net>
2021-10-13 22:07:11 -0700

    fix

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = ef8dedbed0a05a79390f6791d771791c0803e42e
+RPI_FIRMWARE_VERSION = dc99498043298afd6a18629add8580c65a34e779
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
