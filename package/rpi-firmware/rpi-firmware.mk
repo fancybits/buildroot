a8ff0c339ae2919d076fc7827c4524207688d7c4
Aman Karmani <aman@tmm1.net>
2021-10-13 17:26:43 -0700

    bump rpi firmware

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 46f356dcecc5204ca9a8cef7e6894e083b231bc7
+RPI_FIRMWARE_VERSION = ef8dedbed0a05a79390f6791d771791c0803e42e
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
